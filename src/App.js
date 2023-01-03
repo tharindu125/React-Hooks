3d19a7797fc215c0d5583e8fb823bcf9ad40aa9c
tharindu125 <nawanjanaenda@gmail.com>
2023-01-03 21:27:27 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import UseEffect from './Components/UseEffect';
 import UseState from './Components/UseState';
 
 function App() {
   return (
     <div className="App">
       <UseState/>
+      <UseEffect/>
     </div>
   );
 }
--- src/Components/UseEffect.js
+++ src/Components/UseEffect.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function UseEffect() {
+     
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    useEffect(() => {
+        console.log("changed!...")
+    },[count]);
+
+    useEffect(() => {
+        console.log("Change Twice!...")
+    },[count2]);
+
     return(
         <div>
-            
+            <div>
+                <h1>UseEffect</h1>
+            </div>
+            <div>
+                <h1>{count}</h1>
+                <h1>{count2}</h1>
+            </div>
+            <div>
+                <button onClick={() => setCount(count + 1)}>
+                    ADD by one
+                </button>
+                <button onClick={() => setCount2(count +2 )}>
+                    ADD by two
+                </button>
+            </div>
+
         </div>
     )
 }
